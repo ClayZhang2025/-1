1abd6d6b8f4c247cfebe91cec13eb670ea178ac5
ClayZhang2025 <1021038019@qq.com>
2025-02-28 15:10:32 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,27 @@
 const CACHE_NAME = 'homework-counter-cache-v1';
 const urlsToCache = [
-  '.index.html',
-  '.style.css',
-  '.app.js',
-  '.manifest.json',
-  'httpscdn.jsdelivr.netnpm@tensorflowtfjs@3.3.0',
-  'httpscdn.jsdelivr.netnpm@tensorflow-modelscoco-ssd'
+  'index.html',
+  'style.css',
+  'app.js',
+  'manifest.json'
+  // 如果以后需要缓存其他资源，在这里添加
 ];
 
-self.addEventListener('install', event = {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache = {
+      .then(cache => {
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-self.addEventListener('fetch', event = {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response = {
-        return response  fetch(event.request);
+      .then(response => {
+        return response || fetch(event.request);
       })
   );
 });
+
